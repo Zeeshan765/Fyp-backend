a446e99153af9f13769029d4c33b6d73cd7472a3
Zeeshan Ashraf <zeeshanashraf765@gmail.com>
2022-05-24 15:53:48 +0500

    delete change

--- routes/carts.js
+++ routes/carts.js
@@ -1,15 +1,15 @@
-const Cart = require("../models/Cart");
-const User = require("../models/User");
-const express = require("express");
-const mongoose = require("mongoose");
+const Cart = require('../models/Cart');
+const User = require('../models/User');
+const express = require('express');
+const mongoose = require('mongoose');
 //import product
-const Product = require("../models/Product");
-const Component = require("../models/Component");
-const authorization = require("../middleware/authorization");
+const Product = require('../models/Product');
+const Component = require('../models/Component');
+const authorization = require('../middleware/authorization');
 let router = express.Router();
 
 //CREATE
-router.post("/", authorization, async (req, res) => {
+router.post('/', authorization, async (req, res) => {
   console.log(req.body);
 
   let cart = await Cart.findOne({ user: req.user._id });
@@ -22,11 +22,11 @@ router.post("/", authorization, async (req, res) => {
       if (item.product === req.body.product) {
         item.quantity = item.quantity + req.body.quantity;
         await Cart.findOneAndUpdate(
-          { _id: cart._id, "items._id": item._id },
-          { "items.$.quantity": item.quantity }
+          { _id: cart._id, 'items._id': item._id },
+          { 'items.$.quantity': item.quantity }
         );
 
-        return res.status(200).json("Quantity Updated");
+        return res.status(200).json('Quantity Updated');
       }
     }
     //Add new product in the cart
@@ -36,7 +36,7 @@ router.post("/", authorization, async (req, res) => {
       type: req.body.type,
       quantity: req.body.quantity,
     });
-    console.log("cart :", cart);
+    console.log('cart :', cart);
     await cart.save();
     return res.send(cart);
     //res.status(200).json({ message: cart });
@@ -59,53 +59,53 @@ router.post("/", authorization, async (req, res) => {
       // }),
     });
     console.log(cart.user);
-    console.log("cart :", cart);
+    console.log('cart :', cart);
     await cart.save();
     return res.send(cart);
     // }
   }
 });
 //Post Custom Cart
-router.post("/cartarray", authorization, async (req, res) => {
+router.post('/cartarray', authorization, async (req, res) => {
   console.log(req.body.array.length);
   // await Cart.findOneAndDelete({ user: req.user._id });
 
   let cart = await Cart.findOne({ user: req.user._id });
   if (cart) {
     //Checking if Item already in Cart
-    for (item of cart.items) {
-      if (item.product === req.body.product) {
-        item.quantity = item.quantity + req.body.quantity;
-        await Cart.findOneAndUpdate(
-          { _id: cart._id, "items._id": item._id },
-          { "items.$.quantity": item.quantity }
-        );
+    // for (item of cart.items) {
+    //   if (item.product === req.body.product) {
+    //     item.quantity = item.quantity + req.body.quantity;
+    //     await Cart.findOneAndUpdate(
+    //       { _id: cart._id, "items._id": item._id },
+    //       { "items.$.quantity": item.quantity }
+    //     );
 
-        return res.status(200).json("Quantity Updated");
-      }
-    }
+    //     return res.status(200).json("Quantity Updated");
+    //   }
+    //  }
 
     //Add new product in the cart
 
     req.body.array.forEach((element) => {
       if (element) {
-        if (element.category === "Ram") {
+        if (element.category === 'Ram') {
           console.log(element.orgPrice);
           cart.items.push({
             product: element._id,
-            type: "Custom",
+            type: 'Custom',
             quantity: element.price / element.orgPrice,
           });
         } else {
           cart.items.push({
             product: element._id,
-            type: "Custom",
+            type: 'Custom',
             quantity: 1,
           });
         }
       }
     });
-    console.log("cart :", cart);
+    console.log('cart :', cart);
     await cart.save();
     return res.send(cart);
     //res.status(200).json({ message: cart });
@@ -117,24 +117,26 @@ router.post("/cartarray", authorization, async (req, res) => {
       user: req.user._id,
     });
     req.body.array.forEach((element) => {
-      if(element){if (element.category === "Ram") {
-        console.log(element.orgPrice);
-        cart.items.push({
-          product: element._id,
-          type: "Custom",
-          quantity: element.price / element.orgPrice,
-        });
-      } else {
-        cart.items.push({
-          product: element._id,
-          type: "Custom",
-          quantity: 1,
-        });
-      }}
+      if (element) {
+        if (element.category === 'Ram') {
+          console.log(element.orgPrice);
+          cart.items.push({
+            product: element._id,
+            type: 'Custom',
+            quantity: element.price / element.orgPrice,
+          });
+        } else {
+          cart.items.push({
+            product: element._id,
+            type: 'Custom',
+            quantity: 1,
+          });
+        }
+      }
     });
 
     console.log(cart.user);
-    console.log("cart :", cart);
+    console.log('cart :', cart);
     await cart.save();
     return res.send(cart);
     // }
@@ -167,7 +169,7 @@ router.post("/cartarray", authorization, async (req, res) => {
 // });
 
 //get cart length
-router.get("/length", authorization, async (req, res) => {
+router.get('/length', authorization, async (req, res) => {
   let cart = await Cart.findOne({ user: req.user._id });
   if (cart) {
     return res.status(200).json(cart.items.length);
@@ -177,19 +179,19 @@ router.get("/length", authorization, async (req, res) => {
 });
 
 //DELETE cart/Clear Cart
-router.delete("/clear", authorization, async (req, res) => {
+router.delete('/clear', authorization, async (req, res) => {
   try {
     await Cart.findOneAndDelete({ user: req.user._id });
-    res.status(200).json("Cart has been deleted...");
+    res.status(200).json('Cart has been deleted...');
   } catch (err) {
     console.log(error);
-    res.status(500).json("Internal Server Error");
+    res.status(500).json('Internal Server Error');
   }
 });
 
 //Get Custom User Cart
 
-router.get("/cartarray", authorization, async (req, res) => {
+router.get('/cartarray', authorization, async (req, res) => {
   //console.log(req.body);
   try {
     let cart = await Cart.findOne({ user: req.user._id });
@@ -197,17 +199,17 @@ router.get("/cartarray", authorization, async (req, res) => {
     if (cart) {
       //console.log(cart.items[0].quantity);
       if (cart.items.length === 0) {
-        return res.status(200).json("Cart is Empty");
+        return res.status(200).json('Cart is Empty');
       }
     } else {
-      return res.status(200).json("Cart is Empty");
+      return res.status(200).json('Cart is Empty');
     }
     let modedCart = [];
     const length = cart.items.length;
     let count = 0;
     //let ramvalue=4;
     cart.items.forEach(async (item) => {
-      if (item.type === "Custom") {
+      if (item.type === 'Custom') {
         await Component.findById(item.product)
           .then((component) => {
             console.log(component);
@@ -217,8 +219,7 @@ router.get("/cartarray", authorization, async (req, res) => {
               picture: component.picture,
               quantity: item.quantity,
               type: item.type,
-              _id:component._id,
-
+              _id: component._id,
             });
           })
           .catch((err) => {
@@ -239,12 +240,12 @@ router.get("/cartarray", authorization, async (req, res) => {
     });
   } catch (err) {
     console.log(err);
-    res.status(500).json("Internal Server Error");
+    res.status(500).json('Internal Server Error');
   }
 });
 
 //Get Simple Cart
-router.get("/", authorization, async (req, res) => {
+router.get('/', authorization, async (req, res) => {
   //console.log(req.body);
   try {
     let cart = await Cart.findOne({ user: req.user._id });
@@ -252,7 +253,7 @@ router.get("/", authorization, async (req, res) => {
     if (cart) {
       //   //console.log(cart.items[0].quantity);
       if (cart.items.length === 0) {
-        return res.status(200).json("Cart is Empty");
+        return res.status(200).json('Cart is Empty');
       }
     }
     //  else {
@@ -262,7 +263,7 @@ router.get("/", authorization, async (req, res) => {
     const length = cart.items.length;
     let count = 0;
     cart.items.forEach(async (item) => {
-      if (item.type === "Product") {
+      if (item.type === 'Product') {
         await Product.findById(item.product)
           .then((product) => {
             console.log(item._id);
@@ -274,13 +275,13 @@ router.get("/", authorization, async (req, res) => {
               picture: product.picture,
               quantity: item.quantity,
               type: item.type,
-              _id:product._id,
+              _id: product._id,
             });
           })
           .catch((err) => {
             console.log(err);
           });
-      } else if (item.type === "Component") {
+      } else if (item.type === 'Component') {
         await Component.findById(item.product)
           .then((component) => {
             console.log(component);
@@ -291,7 +292,7 @@ router.get("/", authorization, async (req, res) => {
               picture: component.picture,
               quantity: item.quantity,
               type: item.type,
-              _id:component._id,
+              _id: component._id,
             });
           })
           .catch((err) => {
@@ -312,11 +313,11 @@ router.get("/", authorization, async (req, res) => {
     });
   } catch (err) {
     console.log(err);
-    res.status(500).json("Internal Server Error");
+    res.status(500).json('Internal Server Error');
   }
 });
 //Get Complete Cart
-router.get("/complete", authorization, async (req, res) => {
+router.get('/complete', authorization, async (req, res) => {
   //console.log(req.body);
   try {
     let cart = await Cart.findOne({ user: req.user._id });
@@ -324,7 +325,7 @@ router.get("/complete", authorization, async (req, res) => {
     if (cart) {
       //console.log(cart.items[0].quantity);
       if (cart.items.length === 0) {
-        return res.status(200).json("Cart is Empty");
+        return res.status(200).json('Cart is Empty');
       }
     }
     // else {
@@ -334,7 +335,7 @@ router.get("/complete", authorization, async (req, res) => {
     const length = cart.items.length;
     let count = 0;
     cart.items.forEach(async (item) => {
-      if (item.type === "Product") {
+      if (item.type === 'Product') {
         await Product.findById(item.product)
           .then((product) => {
             console.log(product);
@@ -344,13 +345,13 @@ router.get("/complete", authorization, async (req, res) => {
               picture: product.picture,
               quantity: item.quantity,
               type: item.type,
-              _id:product._id,
+              _id: product._id,
             });
           })
           .catch((err) => {
             console.log(err);
           });
-      } else if (item.type === "Component") {
+      } else if (item.type === 'Component') {
         await Component.findById(item.product)
           .then((component) => {
             console.log(component);
@@ -360,13 +361,13 @@ router.get("/complete", authorization, async (req, res) => {
               picture: component.picture,
               quantity: item.quantity,
               type: item.type,
-              _id:component._id,
+              _id: component._id,
             });
           })
           .catch((err) => {
             console.log(err);
           });
-      } else if (item.type === "Custom") {
+      } else if (item.type === 'Custom') {
         await Component.findById(item.product)
           .then((component) => {
             console.log(component);
@@ -376,7 +377,7 @@ router.get("/complete", authorization, async (req, res) => {
               picture: component.picture,
               quantity: item.quantity,
               type: item.type,
-              _id:component._id,
+              _id: component._id,
             });
           })
           .catch((err) => {
@@ -397,27 +398,29 @@ router.get("/complete", authorization, async (req, res) => {
     });
   } catch (err) {
     console.log(err);
-    res.status(500).json("Internal Server Error");
+    res.status(500).json('Internal Server Error');
   }
 });
 
 //Delete Particular Item from the cart
 
-router.patch("/deleteItem/:id", authorization, async (req, res) => {
+router.patch('/deleteItem/:id', authorization, async (req, res) => {
   try {
     let cart = await Cart.findOne({ user: req.user._id });
     if (cart) {
+      console.log('1');
       //This remove cart from database
-      if (cart.items.length <= 1) {
-        await Cart.findOneAndDelete({ _id: cart._id });
-        return res.send("Item Deleted");
-      }
+      // if (cart.items.length <= 1) {
+      //   await Cart.findOneAndDelete({ _id: cart._id });
+      //   return res.send("Item Deleted");
+      // }
       for (i = 0; i < cart.items.length; i++) {
-        //console.log(cart.items[i]._id.toString());
-        if (cart.items[i]._id.toString() === req.params.id) {
+        console.log(cart.items[i].product.toString());
+        if (cart.items[i].product.toString() === req.params.id) {
+          console.log(cart.items[i]._id);
           cart.items.splice(i, 1);
           await Cart.findOneAndUpdate({ _id: cart._id }, { items: cart.items });
-          return res.send("Item Deleted Successfully");
+          return res.send('Item Deleted Successfully');
         }
       }
     }
@@ -440,27 +443,27 @@ router.patch("/deleteItem/:id", authorization, async (req, res) => {
 // })
 
 //UPDATE Increment
-router.patch("/increment/:id", authorization, async (req, res) => {
+router.patch('/increment/:id', authorization, async (req, res) => {
   try {
     let cart = await Cart.findOne({ user: req.user._id })
-      .populate("items.product")
+      .populate('items.product')
       .lean();
     if (cart) {
       for (item of cart.items) {
         if (item._id.toString() === req.params.id) {
-          console.log("1");
+          console.log('1');
           item.quantity = item.quantity + 1;
           await Cart.findOneAndUpdate(
-            { _id: cart._id, "items._id": item._id },
-            { "items.$.quantity": item.quantity }
+            { _id: cart._id, 'items._id': item._id },
+            { 'items.$.quantity': item.quantity }
           );
-          return res.send("Item Decreased");
+          return res.send('Item Decreased');
         } else {
-          return res.status(400).send("Item Not  Decreased");
+          return res.status(400).send('Item Not  Decreased');
         }
       }
 
-      return res.status(400).send("Unable to perform Operation");
+      return res.status(400).send('Unable to perform Operation');
     }
   } catch (error) {
     console.log(error);
@@ -472,27 +475,27 @@ router.patch("/increment/:id", authorization, async (req, res) => {
 
 //Update Cart Item Decrement
 
-router.patch("/decrement/:id", authorization, async (req, res) => {
+router.patch('/decrement/:id', authorization, async (req, res) => {
   try {
     let cart = await Cart.findOne({ user: req.user._id })
-      .populate("items.product")
+      .populate('items.product')
       .lean();
     if (cart) {
       for (item of cart.items) {
         if (item._id.toString() === req.params.id) {
-          console.log("1");
+          console.log('1');
           item.quantity = item.quantity - 1;
           await Cart.findOneAndUpdate(
-            { _id: cart._id, "items._id": item._id },
-            { "items.$.quantity": item.quantity }
+            { _id: cart._id, 'items._id': item._id },
+            { 'items.$.quantity': item.quantity }
           );
-          return res.send("Item Decreased");
+          return res.send('Item Decreased');
         } else {
-          return res.status(400).send("Item Not  Decreased");
+          return res.status(400).send('Item Not  Decreased');
         }
       }
 
-      return res.status(400).send("Unable to perform Operation");
+      return res.status(400).send('Unable to perform Operation');
     }
   } catch (error) {
     console.log(error);
@@ -527,7 +530,7 @@ router.patch("/decrement/:id", authorization, async (req, res) => {
 
 // //GET ALL
 
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const carts = await Cart.find();
     res.status(200).json(carts);
