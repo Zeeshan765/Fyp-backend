fd5bd9a31e2d36a112de0a1bbec2b7e9aee91a67
Sher-Ali180 <sherbat60@gmail.com>
2022-06-21 12:31:09 +0500

    11351

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.send('Backend is Ruunig here ok 1992');
+  res.send('Backend is Running here ok 1992');
 });
 
 module.exports = router;
